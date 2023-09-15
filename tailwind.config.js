5c2f42e302ab024dbabe06197a9c162e60e9b33a
jkim <jonykim@gmail.com>
2023-09-14 21:57:50 -0500

    add nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,13 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'brand-gray-1': '#dadce0',
+        'brand-blue-1': '#1967d2',
+        'brand-green-1': '#137333'
       }
     }
   },
-  plugins: []
+  plugins: ['prettier-plugin-tailwindcss']
 }
