abde1482cdfc598eed0959b6bdaaeaeabce8377c
jkim <jonykim@gmail.com>
2023-09-15 13:58:41 -0500

    add menu items and button to nav bar

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="border-0 rounded px-5 py-2 font-medium text-white bg-brand-blue-1 hover:shadow-blue"
+  >
+    Sign In
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ActionButton'
+}
+</script>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,21 +1,45 @@
 <template>
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
-      <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
-        <h2 class="ml-8 flex h-full items-center">Developed by {{ author }}</h2>
+      <div
+        class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 justify-between"
+      >
+        <div class="flex">
+          <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+          <nav class="ml-12">
+            <ul class="flex h-full list-none">
+              <li
+                v-for="menuItem in menuItems"
+                :key="menuItem"
+                class="flex ml-9 h-full items-center first:ml-0"
+              >
+                <a href="">{{ menuItem }}</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="ml-auto flex h-full items-center">
+          <action-button />
+          <h2 class="ml-8 flex h-full items-center">Developed by {{ author }}</h2>
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from '@/components/ActionButton.vue'
 export default {
   name: 'MainNav',
+  components: {
+    ActionButton
+  },
   data() {
     return {
-      company: 'Dev Jobs',
-      author: 'metalbearsolid'
+      company: 'Solid Jobs',
+      author: 'metalbearsolid',
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Locations', 'Life at Solid Corp', 'How we hire', 'Students', 'Jobs']
     }
   }
 }
