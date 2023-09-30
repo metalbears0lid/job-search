a67fd3c59549f5d8962fb4542b474bf5e18fd7a3
metalbearsolid <jonykim@gmail.com>
2023-09-30 08:04:04 -0500

    refactor text input to be its own component and emit input values to parent

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -4,12 +4,7 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="flex h-full flex-1 items-center pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software engineer"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input placeholder="Software engineer" @handle-input="updateRole" />
       </div>
 
       <span
@@ -19,12 +14,7 @@
 
       <div class="flex h-full flex-1 items-center pl-3">
         <label class="absolute left-0 -top-10">Where></label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Austin"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input placeholder="Los Angeles" @handle-input="updateLocation" />
       </div>
     </div>
 
@@ -34,13 +24,23 @@
 
 <script>
 import ActionButton from '@/components/Shared/ActionButton.vue'
+import TextInput from '@/components/Shared/TextInput.vue'
+
 export default {
   name: 'JobSearchForm',
-  components: { ActionButton },
+  components: { ActionButton, TextInput },
   data: () => {
     return {
       role: '',
       location: ''
+    }
+  },
+  methods: {
+    updateRole(payload) {
+      this.role = payload
+    },
+    updateLocation(payload) {
+      this.location = payload
     }
   }
 }
