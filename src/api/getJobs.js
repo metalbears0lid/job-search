cbd4ae6fb2ff27fa36e03241a20250da884e6ce0
jkim <jonykim@gmail.com>
2023-10-28 10:19:51 -0500

    add env variable for api url

--- src/api/getJobs.js
+++ src/api/getJobs.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 async function getJobs() {
   const baseUrl = import.meta.env.VITE_APP_API_URL
+  console.log(baseUrl)
   const response = await axios.get(`${baseUrl}/jobs`)
   console.log('Received jobs data from backend', response.data)
   return response.data
