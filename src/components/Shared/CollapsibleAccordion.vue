194ae88990a24522441dab16c074220ff39d224f
metalbearsolid <jonykim@gmail.com>
2023-10-28 19:13:11 -0500

    icon to indicate whether accordian is open or close

--- src/components/Shared/CollapsibleAccordion.vue
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="border-b border-solid border-brand-gray-2 py-5">
-    <div class="flex flex-wrap items-center justify-between cursor-pointer" @click="open">asdf</div>
-    <div v-if="isOpen">Child</div>
+    <div class="flex flex-wrap items-center justify-between cursor-pointer" @click="open">
+      <h3 class="text-base font-semibold">Organizations</h3>
+      <font-awesome-icon :icon="caretIcon" />
+    </div>
+    <div v-if="isOpen" class="mt-5 w-full">Child</div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const isOpen = ref(false)
+
+const caretIcon = computed(() => {
+  return isOpen.value ? ['fas', 'angle-up'] : ['fas', 'angle-down']
+})
 
 function open() {
   isOpen.value = !isOpen.value
